8642d08fb0224a88e5658d1038102c1f83b8ad07
pqasmi <pmqas002@gmail.com>
2021-08-11 21:14:26 -0400

    update zombie and player objects

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
+let ammunition = 0
+let coins = 0
+let reload = null
+
+
 console.log("Zombie Hunt!!!")
+
+
+
+
+
+class Game {
+constructor (name, ) {
+    this.name = name
+}
+
+
+}
+
+let player = new Game
+
+
